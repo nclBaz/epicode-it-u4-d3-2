53b5e6441094f20f67b41e1aa9583979bf5feb24
Riccardo Gulin <riccardo.gulin@gmail.com>
2023-06-28 11:51:41 +0200

    Adds loops examples

--- src/u4d3/App.java
+++ src/u4d3/App.java
@@ -51,32 +51,71 @@ public class App {
 //			System.out.println("Nessuno dei precedenti");
 //		}
 
-		int x = 10, y = 20;
-		int z = (x > y) ? -100 : 100;
+//		int x = 10, y = 20;
+//		int z = (x > y) ? -100 : 100;
+//
+//		switch (z) {
+//		default:
+//			System.out.println("Default");
+//			x--;
+//			break;
+//		case 30:
+//			x++;
+//			break;
+//		case -100:
+//			System.out.println("Il numero è -100");
+//			break;
+//
+//		case 100:
+//			System.out.println("Il numero è 100");
+//			x--;
+//			break;
+//		case 1000:
+//			System.out.println("Il numero è 1000");
+//			x--;
+//			break;
+//		}
+//
+//		System.out.println(x);
 
-		switch (z) {
-		default:
-			System.out.println("Default");
-			x--;
-			break;
-		case 30:
-			x++;
-			break;
-		case -100:
-			System.out.println("Il numero è -100");
-			break;
+		// ***************************** LOOPS **********************
+//
+//		int x = 6, y = 3;
+//
+//		while (y <= x + 6) {
+//			System.out.println(y);
+//			y += 3;
+//		}
+//
+//		do {
+//			System.out.println(x++);
+//		} while (x < 10);
 
-		case 100:
-			System.out.println("Il numero è 100");
-			x--;
-			break;
-		case 1000:
-			System.out.println("Il numero è 1000");
-			x--;
-			break;
-		}
+//		for (int i = 0; i < 10; i++) {
+//			if (i % 2 == 0)
+//				continue; // col continue saltiamo l'iterazione corrente e passiamo alla successiva
+//			System.out.println(i);
+//		}
+//
+//		for (int i = 0; i < 10; i++) {
+//			if (i == 5)
+//				break; // con il break usciamo dal loop
+//			System.out.println(i);
+//		}
 
-		System.out.println(x);
+//		esterno:
+//		for (int i = 1; i <= 5; i++) {
+//
+//			if (i == 3)
+//				continue; // salto
+//
+//			for (int j = 1; j <= 5; j++) {
+//				if (j == 4)
+//					break esterno; // esci dal ciclo esterno
+//				System.out.println("j:" + j);
+//			}
+//			System.out.println("i:" + i);
+//		}
 
 	}
 
